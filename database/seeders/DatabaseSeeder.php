03cd153f5f290a45892a1c357b0b636627346147
ZiadBadr1 <103049045+ZiadBadr1@users.noreply.github.com>
2023-06-16 12:18:15 +0300

    add user seeder and task factory to test the website

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+            \App\Models\Task::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+        UserSeeder::class
+            ]);
     }
 }
